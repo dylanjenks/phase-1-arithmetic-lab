00d9cea3758ab5d6454628203153d8f9d7307e49
dylanjenks <dylan.jenks92@gmail.com>
2021-09-20 16:47:46 -0400

    commit tree e58df38b4e46ad8d482676d7d64ea73138f0fd60

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+{ const num1 = 1
+const num2 = 62
+let multiply = num1 * num2 }
 
-// Write your code here
+{let random = Math.floor(Math.random() * 10) + 1;}
+
+{const num3 = 8
+    const num4 = 4
+    let mod = num3 - num4}
+
+{const max = Math.max(1, 2, 3, 4, 5, 20);}
